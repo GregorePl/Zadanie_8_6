bc81f5fe48689b78a5a5c293398e37ef3a9f60cb
gregveg <kuzniak.greg@gmail.com>
2017-08-29 20:56:30 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var a = 4;
+var b = 5;
+var value = (a * a) + (2 * a * b) - (b * b);
+    if (value > 0) {
+        
+        console.log ('wynik dodatni')
+    }
+    else if (value < 0) {
+        console.log ('wynik ujemny')
+    }
+    if (value === 0) {
+        console.log ('wynik równy zero')
+    }
